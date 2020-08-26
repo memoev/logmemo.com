9f1f4d91a675fad11d3970c26420fb9714abb1e9
Guillermo Villalta <mexcelus@gmail.com>
2020-08-26 17:30:33 -0600

    :art: jsconfig
    adding jsconfig file - trying to get intellisense working

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,14 @@ import React from "react"
 
 import NavBar from "../components/NavBar"
 import Footer from "../components/Footer"
-import SubForm from "../components/SubForm"
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <SubForm />
+      <div className="main">
+        
+      </div>
       <Footer />
     </>
   )
