b3c81d9bf9750b31a49d55b8430a8214fd5cd991
Guillermo Villalta <mexcelus@gmail.com>
2020-08-28 16:03:53 -0600

    :sparkles: social links
    added social images to redirect users to my social pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from "react"
 
+import github from '../../static/github.png'
+import twitter from '../../static/twitter.jpg'
+import linkedin from '../../static/linkedin.jpg'
+
 export default function Footer() {
   return (
     <footer>
-      <a href="/">Click Me</a>
+      <div className="center">
+        <img className='footer-img' src={ github } alt='github'></img>
+        <img className='footer-img' src={ twitter } alt='github'></img>
+        <img className='footer-img' src={ linkedin } alt='github'></img>
+      </div>
     </footer>
   )
 }
