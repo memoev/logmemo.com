4655764a5b895e80094df5ad6d37dfb68d3560a7
Guillermo Villalta <mexcelus@gmail.com>
2020-08-27 18:08:56 -0600

    :speech_balloon: personal info
    ps about me?

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,17 @@ export default function Home() {
     <>
       <NavBar />
       <Ribbon />
+      <div className="me">
+        <p>
+          <b>Hi!</b> <span role="img" aria-label="hand">🖖🏽</span> 
+        </p>
+        <p>
+          My name is Guillermo Villalta. I'm a software developer trying to share and document my journey throughout web development.
+        </p>
+        <p>
+          This is a personal space where I'm planning to share my notes, thoughts and articles about anything that revolves around technology and programming.
+        </p>
+      </div>
       <Footer />
     </>
   )
