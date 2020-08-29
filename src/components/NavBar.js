ee4bc91f27d992de002ebcb088fe546466e41faf
Guillermo Villalta <mexcelus@gmail.com>
2020-08-28 18:11:51 -0600

    :zap: changed anchor for gatsby link
    improved navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function NavBar() {
   return (
     <nav className="navbar">
       <div className="center">
-        <a className="nav-links" href="/">memo</a>|
-        <a className="nav-links" href="/">blog</a>
+        <Link className="nav-links" to="/">memo</Link>|
+        <Link className="nav-links" to="/blog">blog</Link>
       </div>
     </nav>
   )
