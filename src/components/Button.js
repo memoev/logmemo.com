01b7c09ef3e3bad09d807d98c5edca89c42dbaf0
Guillermo Villalta <mexcelus@gmail.com>
2020-08-28 18:10:44 -0600

    :sparkles: button component
    create button component to handle buttons depending if link or just button

---
+++ src/components/Button.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Link } from "gatsby"
+
+
+export default function Footer({ to, children }) {
+    const styles = {
+        display: 'inline-block',
+        color: '#00897b',
+        border: 'none',
+        background: '#00db8b',
+        borderRadius: '1em',
+        padding: '10px',
+        cursor: 'pointer',
+        margin: '10px'
+    }
+
+    return (
+    to ? (
+    <Link to={to} style={styles}>{ children }</Link>
+    ) : (
+    <button style={styles}>{ children }</button>
+    )
+  )
+}
