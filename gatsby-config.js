03895218a079788da3acf77d4603b701355b31fa
Guillermo Villalta <mexcelus@gmail.com>
2020-08-31 16:13:51 -0600

    :wrench: adding plugins
    markdown plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/content/blog`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 }
