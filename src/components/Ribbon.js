f8063cb562d095d0a9e6a3b78584baaa983b16b1
Guillermo Villalta <mexcelus@gmail.com>
2020-11-13 10:00:22 -0700

    logo change

--- src/components/Ribbon.js
+++ src/components/Ribbon.js
@@ -2,13 +2,11 @@ import React from "react"
 
 import DarkMode from "./DarkMode"
 
-import memo from "../../static/memologo.png"
-import arrows from "../../static/arrows.png"
+import memo from "../../static/rosepen.png"
 
 export default function Ribbon() {
   return (
     <div className="main">
-        <img className="arrows" src={ arrows } alt="arrows"></img>
         <span className="hover">
           <img className="logo" src={ memo } alt="logo"></img>
           <span className="header">guillermo's journey into design & development</span>
