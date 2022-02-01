15965c98306c779ac0fadc5d7ca47c9ef83ce5f2
Guillermo Villalta <mexcelus@gmail.com>
2022-01-31 23:00:35 -0700

    small changes

--- src/components/Ribbon.js
+++ src/components/Ribbon.js
@@ -8,8 +8,8 @@ export default function Ribbon() {
   return (
     <div className="main">
         <span className="hover">
-          <img className="logo" src={ memo } alt="logo"></img>
-          <span className="header">guillermo's journey into design & development</span>
+          {/* <img className="logo" src={ memo } alt="logo"></img> */}
+          <h2 className="header">hello there!<span role="img" aria-label="rocket">🚀</span></h2>
         </span>
         <DarkMode />
     </div>
